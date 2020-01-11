26dba066aecf5429e4d2b55a4494ad6ac51e97e9
Kai Johnson <kai.johnson@eagleview.com>
2020-01-11 12:49:24 -0800

    adds makefile that handles creating, connecting, and tearing down the database

---
+++ ci/db/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# flag to fail installation if
+set -e
+
+# pulls latest postgres image
+docker pull postgres:12.1
+
+docker run -d \
+    --name betany-db \
+    -e POSTGRES_PASSWORD=password \
+    -p 5432:5432 \
+    postgres:12.1
+
+echo 'waiting for postgres DB to complete setup...'
+sleep 5
+
+# checks to see if pgcli is installed. If not, brew install it.
+if type pgcli >/dev/null 2>&1; then
+    echo 'pgcli already installed...'
+else
+    echo 'installing pgcli...'
+    brew install pgcli
+fi
---
+++ makefile
@@ -0,0 +1,13 @@
+PHONY: db
+PG_DEV_PASSWORD=password
+
+db:
+	echo 'setting up local DB'
+	sh ci/db/setup.sh
+
+connect-db:
+	export PGPASSWORD=$(PG_DEV_PASSWORD) && pgcli -h localhost -p 5432 -U postgres
+
+cleanup-db:
+	docker kill betany-db
+	docker rm betany-db
