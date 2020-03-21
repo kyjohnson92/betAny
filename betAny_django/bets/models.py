3c76c603048a3bc09159844f93ab920635cfadec
Kyle Johnson <ky.j.johnson92@gmail.com>
2020-03-21 13:26:04 -0400

    added user login auth

--- betAny_django/bets/models.py
+++ betAny_django/bets/models.py
@@ -4,15 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    firstName = models.CharField(null=False, max_length=25)
-    lastName = models.CharField(null=False, max_length=30)
-    assets = models.DecimalField(decimal_places=2, max_digits=20, default=0.00)
+    account_balance = models.DecimalField(decimal_places=2, max_digits=20, default=0.00)
 
     class Meta:
         ordering = ['user']
 
     def __str__(self):
-        return str(self.user)
+        userName = self.user.get_full_name()
+        return f"{userName} Profile"
         
     objects = models.Manager()
 
--- betAny_django/main/urls.py
+++ betAny_django/main/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('bets.urls')),
     path('', include('bets.api.urls')),
 ]
