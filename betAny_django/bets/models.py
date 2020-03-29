59c38c2d02988f138c9fa77d64f63039e045e940
Kyle Johnson <ky.j.johnson92@gmail.com>
2020-03-29 13:10:55 -0400

    update bets to be only positive amounts

--- betAny_django/bets/models.py
+++ betAny_django/bets/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class UserProfile(models.Model):
@@ -20,7 +21,7 @@ class Bet(models.Model):
     betID = models.AutoField(primary_key=True)
     bettorID = models.ForeignKey(User, related_name='party1', on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
-    bettor_amount = models.DecimalField(decimal_places=2, max_digits=20)
+    bettor_amount = models.DecimalField(decimal_places=2, max_digits=20,validators=[MinValueValidator(0.01)])
     betteeID = models.ForeignKey(User, related_name='party2', on_delete=models.CASCADE, blank=True, null=True)
     betOutcome = models.CharField(max_length=20, choices=[('0',''),('1', str(bettorID) + '__Wins'),('2',str(betteeID) +'__Wins')], default='0')
     
