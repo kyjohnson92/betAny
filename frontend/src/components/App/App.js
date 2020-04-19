f7f272bb4329c6fd54e9152f870ae853141acc42
Brian Monaccio <brianmonaccio@gmail.com>
2020-04-19 12:15:29 -0400

    Adds react-helmet and pulls in bootstrap css from CDN

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { loginUser, logout, selectUser } from '../../store/user/userSlice';
@@ -32,6 +33,14 @@ function App() {
 
   return (
     <Router>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous"
+        />
+      </Helmet>
       <Navbar />
       <div>
         {user.isLoggedIn ? (
